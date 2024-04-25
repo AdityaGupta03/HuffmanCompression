050d2e3558246bda0f0c28246bfdf21637aeb758
Aditya Gupta <adgupta1003@gmail.com>
2024-04-25 01:18:44 -0400

    Read input file contents.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

--- src/FileCompression.cpp
+++ src/FileCompression.cpp
@@ -10,5 +10,14 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
+    // Read the contents of the input file
+    std::fstream in_file(argv[1], std::ios::in);
+    std::stringstream contents_stream;
+    contents_stream << in_file.rdbuf();
+    std::string contents = contents_stream.str();
+    in_file.close();
+
+    std::cout << contents << std::endl;
+
     return EXIT_SUCCESS;
 }
